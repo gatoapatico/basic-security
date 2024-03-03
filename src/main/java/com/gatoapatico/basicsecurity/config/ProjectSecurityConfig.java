0b437dadbb74f978e8e8a5ada8d620785d000590
ALVARO FERNANDEZ <aferzu@gmail.com>
2024-03-02 23:27:01 -0500

    Initial commit

---
+++ src/main/java/com/gatoapatico/basicsecurity/config/ProjectSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.gatoapatico.basicsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                    .requestMatchers("/myAccount").authenticated()
+                    .requestMatchers("/notices").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
